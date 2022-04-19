c407203bf419496dcf06f381caaaf99a62681709
BRUNO ANDWELE ALVES ANTUNES <bruno.3a@outlook.com>
2022-04-19 20:25:47 -0300

    Adicionei a movimentação do arroz usando as teclas [w,a,s,d]

--- Jogo/js1.js
+++ Jogo/js1.js
@@ -3,10 +3,8 @@ let canvas = document.getElementById("my_Canvas");
 let ctx = canvas.getContext("2d");
 
 
-
-//-------------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------
-
+//#############################################################################################################
+//#############################################################################################################
 
 
 //Adicionando evento para pegar os valores das teclas pressionadas;
@@ -29,15 +27,48 @@ function soltar(e){
 }
 
 
+//#############################################################################################################
+//#############################################################################################################
 
+
+//Função para movimentar o jogador 1 (w,a,s,d):
+
+//Aqui adicionei a imagem do arroz;
+let arroz = new Image(); arroz.src= 'arroz.png'; let arroz_speed=5; let x_arroz = 300; let y_arroz=300;
+
+function j1_move(){
+		/*Condições para ver se uma tecla está sendo pressionada (ele verifica se a tecla está dentro do vetor
+			"keys" e então executa a função*/
+	if ( 'w' in keys ){
+		y_arroz -= arroz_speed;
+	}
+	if ( 's' in keys ){
+		y_arroz += arroz_speed;
+	}
+	if ( 'd' in keys ){
+		x_arroz += arroz_speed;
+	}
+	if ( 'a' in keys ){
+		x_arroz -= arroz_speed;
+	}
+
+	ctx.beginPath(); //Para indicar o começo
+	ctx.drawImage(arroz, x_arroz, y_arroz, 80, 80); //Para desenhar o arroz de acordo com suas novas posições
+}
+
+
+//#############################################################################################################
 //-------------------------------------------------------------------------------------------------------------
+//#############################################################################################################
 //-------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------
-//------------------------------------------------------------
-//------------------------------------------------------------
-//------------------------------------------------------------
-//------------------------------------------------------------
+//#############################################################################################################
+
+
 //Função principal:
 function main(){
-} main()
+	requestAnimationFrame(main); //Para pedir que o site chame novamente essa função toda vez que atualizar
+	ctx.beginPath(); ctx.clearRect(0, 0, canvas.width, canvas.height);//Limpa o canvas para a proxima animação
+	j1_move(); //chamando a função do jogador1 (arroz)
+}
+main(); //Chamando a função principal.
 

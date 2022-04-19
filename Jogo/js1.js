bfe5f88a7fd803f44be458899271991943cee501
brunoandwele <bruno.3a@outlook.com>
2022-04-19 17:16:04 -0300

    inicio javascript

---
+++ Jogo/js1.js
@@ -0,0 +1,15 @@
+let canvas = document.getElementById('my_Canvas');
+let ctx = canvas.getContext('2d');
+let tecla = [];
+//Capturar movimentos do teclado:
+
+document.addEventListener('keydown', function(e){
+    tecla[e.keyCode] = true;
+    
+});
+
+
+
+
+
+

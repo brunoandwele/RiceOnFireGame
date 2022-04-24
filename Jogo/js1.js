0da083f88f615b3b95e514b22ec6dcbe0f310514
brunoandwele <bruno.3a@outlook.com>
2022-04-23 21:44:33 -0300

    Bug de colisão arrumado/Pontuação e vidas add html
    HTML:
    1. Criei uma div nova dentro da DIV do título para coloar o título em si e as imagens e na div maior adicionei uma nova tag "p" em que nela foram adicionados o html necessário para adicionar os pontos e as vidas.
    
    ######################
    ######################
    
    CSS:
    1.Só ajustei o css para ficar bonitinho as divs e na nova tag P, caso queira ver são as de id "titullo_img" e id "pontos"
    
    #################
    #################
    
    JS:
    Foi onde eu mais mexi!
    
    1.Adicionei uma variável para inserir a vida e os pontos no html!
    
    2. Centralizei a posição inicial do arroizin para o centro do canvas
    
    3. Arrumei as delimitações das colisões do arroz com a borda e com a comida
    
    4. Arrumei o bug da comida aparecer em lugares que o arroz não consegue pegar

--- Jogo/js1.js
+++ Jogo/js1.js
@@ -30,7 +30,12 @@ function soltar(e){
 //-------------------------------------------------------------------------------------------------------------
 //#############################################################################################################
 // Função resposanvel por contabilizar o número de vidas:
-let num_vidas = 5
+
+//Criando variáveis para que seja possível adicionar as informações de vida no html
+let vidas_restantes = document.getElementById('vidas'); //Cria a variável como o texto da tag de id vidas
+let num_vidas = 5 //Define com 5 o número inicial de vidas!!!!
+vidas_restantes.innerText = num_vidas.toString() //Adiciona pela primeira vez no html o valor das vidas, o qual começa no 5!
+
 function vidas(){
 	if (num_vidas != 0)
 	{
@@ -50,7 +55,7 @@ function vidas(){
 
 
 //Aqui adicionei a imagem do arroz;
-let arroz = new Image(); arroz.src= 'arrozin.png'; let arroz_speed=8; let x_arroz = 300; let y_arroz=300;
+let arroz = new Image(); arroz.src= 'arrozin.png'; let arroz_speed=8; let x_arroz = (canvas.width/2); let y_arroz=(canvas.height/2);
 
 function j1_move(){
 		/*Condições para ver se uma tecla está sendo pressionada (ele verifica se a tecla está dentro do vetor
@@ -71,43 +76,47 @@ function j1_move(){
 	/*Delimitação de espaço do personagem dentro do canvas - 4 delimitações para 4 lados*/
 	/* Utiliza-se como referencial a largura e comprimento do Canvas, a partir disso o eixo x ou y assumirá seu valor decrementado*/
 	/*Lateral direita*/
-	if (x_arroz > (canvas.width - 80)){
+	if (x_arroz-40 >= (canvas.width - 80)){
         //x_arroz = canvas.width - 80;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
+		vidas_restantes.innerText = num_vidas.toString()//Atualiza no HTML o valor do número de vidas
 		console.log(num_vidas);
     }
 
 	/*Lateral esquerda*/
-	if (x_arroz < (canvas.width - 650)){
-		//x_arroz = canvas.width - 650;
+	if (x_arroz <= (40)){
+		//x_arroz = 40;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
+		vidas_restantes.innerText = num_vidas.toString()//Atualiza no HTML o valor do número de vidas
 		console.log(num_vidas);
 		
 	}
 
 	/*Inferior*/
-	if (y_arroz > (canvas.height - 80)){
+	if (y_arroz-40 >= (canvas.height - 80)){
 		//y_arroz = canvas.height - 80;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
+		vidas_restantes.innerText = num_vidas.toString() //Atualiza no HTML o valor do número de vidas
 		console.log(num_vidas);
 		
    	}
    
 	/*Superior*/
-   	if (y_arroz < (canvas.height - 520)){
-		//y_arroz = canvas.height - 520;
+   	if (y_arroz <= (40)){
+		//y_arroz = 40;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
+		vidas_restantes.innerText = num_vidas.toString()//Atualiza no HTML o valor do número de vidas
 		console.log(num_vidas);
 	}
 
 	
 
 	ctx.beginPath(); //Para indicar o começo
-	ctx.drawImage(arroz, x_arroz, y_arroz, 80, 80); //Para desenhar o arroz de acordo com suas novas posições
+	ctx.drawImage(arroz, x_arroz-40, y_arroz-40, 80, 80); //Para desenhar o arroz de acordo com suas novas posições
 	
 }
 
@@ -121,28 +130,40 @@ function getRandomInt(max) {
 }
 
 //São os elementos da comida! - valores x e y iniciais e fonte da imagem
-let x_comida = getRandomInt(canvas.width-50); let y_comida = getRandomInt(canvas.height-45); //Coloquei como limite 40 pixels a menos da borda(Além do tamanho da foto, para ficar dentro do canvas)
+let x_comida = getRandomInt(canvas.width-60)+30; //Defini um valor inicial aleatório para x
+let y_comida = getRandomInt(canvas.height-50)+25;//Defini um valor inicial aleatório para y
 let comida = new Image(); comida.src = "comidinha.jpeg";
 
+//Criando variáveis para que seja possível adicionar as informações de pontuação no html
+let pontuacao = document.getElementById('pontos'); //Cria a variável como o texto da tag de id pontos
 let pontos = 0; //Cria a variável pontos, para que seja feita a contagem
+
+pontuacao.innerText = pontos //Adiciona pela primeira vez no html o valor da pontuação, o qual começa no 0!
+
 
 function comidinha(){
 	
-	//Verificar a colisão - foi definido como se o arroz fosse apenas um ponto na tela
-	//Ele verifica se o "ponto" arroz entra na área do arroizin
+	//Verificar a colisão - foram definidos 4 pontos da comida, pontos esses que são os cantos do quadrado que forma a imagem;
+	//Ele verifica se os "pontos" da comida entraram na área do arroizin
 	//Para isso o "x" e o "y" da comida precisa estar dentro desses requisitos:
-	if ( (x_comida >= x_arroz) && (x_comida <= (x_arroz+80)) 
-	&& (y_comida >= y_arroz) && (y_comida <= (y_arroz + 80))){
+	if ( ((x_comida >= x_arroz) || (x_comida+30 >= x_arroz) ) &&
+		((x_comida <= x_arroz + 80) || (x_comida+30 <= x_arroz + 80)) && 
+		((y_comida >= y_arroz) || (y_comida+25 >= y_arroz))&&
+		((y_comida <= y_arroz + 80) || (y_comida+25 <= y_arroz+80)) ){
 		
 		pontos += 1; //A pontuação aumenta em 1 ponto!
 		
-		x_comida = getRandomInt(canvas.width-40); //É definido um novo "x"
-		y_comida = getRandomInt(canvas.height-35); //É definido um novo "y"
+		pontuacao.innerText = pontos //Atualiza no HTML a pontuação!
+		
+		x_comida = getRandomInt(canvas.width-60) + 30; //É definido um novo "x"
+		y_comida = getRandomInt(canvas.height-50) + 25; //É definido um novo "y"
 	}
 	
 	
 	ctx.beginPath(); //Inicio do desenho
-	ctx.drawImage(comida, x_comida, y_comida, 30,25); //Desenha a comida
+	ctx.drawImage(comida, x_comida-15, y_comida-12.5, 30,25); //Desenha a comida
+	
+	
 }
 
 

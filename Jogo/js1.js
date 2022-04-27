452af2d832f4da89091d1a95c9977f3bc741fa06
brunoandwele <bruno.3a@outlook.com>
2022-04-27 17:23:08 -0300

    Adicionei o boss e arrumei os comentários
    Adicionei o boss e o sistema de chasing, mas falta adicoonar o sistema de colisão para ele!!!

--- Jogo/js1.js
+++ Jogo/js1.js
@@ -3,8 +3,8 @@ let canvas = document.getElementById("my_Canvas");
 let ctx = canvas.getContext("2d");
 
 
-//#############################################################################################################
-//#############################################################################################################
+//#########################################################
+//#########################################################
 //Adicionando evento para pegar os valores das teclas pressionadas;
 
 
@@ -24,11 +24,8 @@ function soltar(e){
 	delete keys[e.key]; //Após soltar a tecla ele apaga ela da lista
 }
 
-//#############################################################################################################
-//-------------------------------------------------------------------------------------------------------------
-//#############################################################################################################
-//-------------------------------------------------------------------------------------------------------------
-//#############################################################################################################
+//#########################################################
+//#########################################################
 // Função resposanvel por contabilizar o número de vidas:
 
 //Criando variáveis para que seja possível adicionar as informações de vida no html
@@ -37,10 +34,10 @@ let num_vidas = 5 //Define com 5 o número inicial de vidas!!!!
 vidas_restantes.innerText = num_vidas.toString() //Adiciona pela primeira vez no html o valor das vidas, o qual começa no 5!
 
 function vidas(){
-	if (num_vidas != 0)
+	if (num_vidas > 0)
 	{
-		x_arroz = 300;//retorna o bolinho de arroz para a posição inicial
-		y_arroz=300;//retorna o bolinho de arroz para a posição inicial
+		arroz_d.x = 300;//retorna o bolinho de arroz para a posição inicial
+		arroz_d.y=300;//retorna o bolinho de arroz para a posição inicial
 	}
 	else
 	{
@@ -49,34 +46,41 @@ function vidas(){
 	}
 }
 
-//#############################################################################################################
-//#############################################################################################################
+//#########################################################
+//#########################################################
 //Função para movimentar o jogador 1 (w,a,s,d):
 
 
 //Aqui adicionei a imagem do arroz;
-let arroz = new Image(); arroz.src= 'arrozin.png'; let arroz_speed=8; let x_arroz = (canvas.width/2); let y_arroz=(canvas.height/2);
+let arroz = new Image(); arroz.src= 'arrozin.png';
+let arroz_d={
+	speed:8,
+	x: (canvas.width/2),
+	y:(canvas.height/2),
+	w:80,
+	h:80
+}
 
 function j1_move(){
 		/*Condições para ver se uma tecla está sendo pressionada (ele verifica se a tecla está dentro do vetor
 			"keys" e então executa a função*/
 	if ( 'w' in keys ){
-		y_arroz -= arroz_speed;
+		arroz_d.y -= arroz_d.speed;
 	}
 	if ( 's' in keys ){
-		y_arroz += arroz_speed;
+		arroz_d.y += arroz_d.speed;
 	}
 	if ( 'd' in keys ){
-		x_arroz += arroz_speed;
+		arroz_d.x += arroz_d.speed;
 	}
 	if ( 'a' in keys ){
-		x_arroz -= arroz_speed;
+		arroz_d.x -= arroz_d.speed;
 	}
 	
 	/*Delimitação de espaço do personagem dentro do canvas - 4 delimitações para 4 lados*/
 	/* Utiliza-se como referencial a largura e comprimento do Canvas, a partir disso o eixo x ou y assumirá seu valor decrementado*/
 	/*Lateral direita*/
-	if (x_arroz - 40 >= (canvas.width-80)){
+	if (arroz_d.x - arroz_d.w/2 >= (canvas.width-80)){
         //x_arroz = canvas.width - 80;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
@@ -85,7 +89,7 @@ function j1_move(){
     }
 
 	/*Lateral esquerda*/
-	if (x_arroz - 40 <= (0)){
+	if (arroz_d.x - arroz_d.w/2 <= (0)){
 		//x_arroz = 40;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
@@ -95,7 +99,7 @@ function j1_move(){
 	}
 
 	/*Inferior*/
-	if (y_arroz-40 >= (canvas.height-80)){
+	if (arroz_d.y - arroz_d.h/2 >= (canvas.height-80)){
 		//y_arroz = canvas.height - 80;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
@@ -105,23 +109,23 @@ function j1_move(){
    	}
    
 	/*Superior*/
-   	if (y_arroz-40 <= (0)){
+   	if (arroz_d.y - arroz_d.h/2 <= (0)){
 		//y_arroz = 40;
 		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
 		vidas()
 		vidas_restantes.innerText = num_vidas.toString()//Atualiza no HTML o valor do número de vidas
 		console.log(num_vidas);
 	}
-
+	
 	
 
 	ctx.beginPath(); //Para indicar o começo
-	ctx.drawImage(arroz, x_arroz-40, y_arroz-40, 80, 80); //Para desenhar o arroz de acordo com suas novas posições
+	ctx.drawImage(arroz, arroz_d.x - arroz_d.w/2, arroz_d.y - arroz_d.h/2, arroz_d.w, arroz_d.h); //Para desenhar o arroz de acordo com suas novas posições
 	
 }
 
-//#############################################################################################################
-//#############################################################################################################
+//#########################################################
+//#########################################################
 //Comidinhas do arroizin
 
 //Função para gerar um valor aleatório para depois associar ao x e y;
@@ -146,10 +150,10 @@ function comidinha(){
 	//Verificar a colisão - foram definidos 4 pontos da comida, pontos esses que são os cantos do quadrado que forma a imagem;
 	//Ele verifica se os "pontos" da comida entraram na área do arroizin
 	//Para isso o "x" e o "y" da comida precisa estar dentro desses requisitos:
-	if ((x_comida+15 >= x_arroz-40) &&
-		(x_comida+15 <= x_arroz+40) &&
-		(y_comida+12.5 >= y_arroz-40) &&
-		(y_comida+12.5 <= y_arroz+40)){
+	if ((x_comida+15 >= arroz_d.x-40) &&
+		(x_comida+15 <= arroz_d.x +40) &&
+		(y_comida+12.5 >= arroz_d.y-40) &&
+		(y_comida+12.5 <= arroz_d.y+40)){
 		
 		pontos += 1; //A pontuação aumenta em 1 ponto!
 		
@@ -166,8 +170,8 @@ function comidinha(){
 	
 }
 
-//#############################################################################################################
-//#############################################################################################################
+//#########################################################
+//#########################################################
 //Comidinha especial!
 
 let c_especial = new Image(); c_especial.src = 'c_especial.jpeg'; 
@@ -176,7 +180,7 @@ let x_cEspecial = getRandomInt(canvas.width-60) + 30; let y_cEspecial = getRando
 
 
 function cespecial(){
-	let atualizador = getRandomInt(20);
+	let atualizador = getRandomInt(50);
 	
 	if (atualizador === 1 && contador_especial<30) {
 		contador_especial += 1;
@@ -186,10 +190,10 @@ function cespecial(){
 		ctx.beginPath();
 		ctx.drawImage(c_especial , x_cEspecial , y_cEspecial , 30,25);
 		
-		if ((x_cEspecial >= x_arroz-40) &&
-			(x_cEspecial <= x_arroz+40) &&
-			(y_cEspecial >= y_arroz-40) &&
-			(y_cEspecial <= y_arroz+40) ){
+		if ((x_cEspecial >= arroz_d.x-40) &&
+			(x_cEspecial <= arroz_d.x+40) &&
+			(y_cEspecial >= arroz_d.y-40) &&
+			(y_cEspecial <= arroz_d.y+40) ){
 			
 			contador_especial=0;
 			
@@ -198,17 +202,14 @@ function cespecial(){
 			
 			x_cEspecial = getRandomInt(canvas.width);
 			y_cEspecial = getRandomInt(canvas.height);
-		};
-	};
+		}
+	}
 	
 	
-};
+}
 
-//#############################################################################################################
-//-------------------------------------------------------------------------------------------------------------
-//#############################################################################################################
-//-------------------------------------------------------------------------------------------------------------
-//#############################################################################################################
+//#########################################################
+//#########################################################
 //Fogos:
 let fogo = new Image(); fogo.src= 'fogo_pixilizado.png'; let fogo_speed=8; let x_fogo = (canvas.width/2); let y_fogo=(canvas.height/2);
 
@@ -223,9 +224,53 @@ function foguinho(){
 	
 }
 
+//#########################################################
+//#########################################################
+//Boss
 
+let boss = new Image(); boss.src='boss.png'; //Criando e adicionando a imagem do boss
 
+//São os dados do boss
+let boss_d = {
+	h:80,
+	w:50,
+	x:(canvas.width/2),
+	y:80,
+	speed: 3
+}
 
+//Ele perseguirá o arroizin, então é necessário fazer um sistema de chasing comparando os x e y
+function boss_move(pontos){
+	if (pontos>=0) {
+		//Condições para caçar o arroz
+		if ( arroz_d.x > boss_d.x ) {
+			boss_d.x += boss_d.speed;
+		} else {
+			boss_d.x -= boss_d.speed;
+		}
+		
+		if ( arroz_d.y > boss_d.y ) {
+			boss_d.y += boss_d.speed;
+		} else {
+			boss_d.y -= boss_d.speed;
+		}
+		
+		ctx.beginPath();
+		ctx.drawImage(boss, boss_d.x - boss_d.w / 2, boss_d.y - boss_d.h / 2, boss_d.w, boss_d.h); //Desenha o arroz de acordo com seus dados
+		
+	}
+	
+	//Colisao
+	/*
+	if (
+		
+	){
+		num_vidas -=1;//Contabiliza o número de vidas que foram perdidas ao escostar na parede
+		vidas()
+		vidas_restantes.innerText = num_vidas.toString() //Atualiza no HTML o valor do número de vidas
+		console.log(num_vidas);
+	}*/
+}
 
 
 //#############################################################################################################
@@ -243,6 +288,7 @@ function main(){
 	cespecial();
 	j1_move(); //chamando a função do jogador1 (arroz)
 	foguinho();
+	boss_move(pontos);
 }
 
 main(); //Chamando a função principal.
